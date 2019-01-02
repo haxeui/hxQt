ed82912a6eee024746e4ae5faf9595fcedbda29a
Ian Harrigan <ianharrigan@hotmail.com>
2019-01-02 20:01:55 +0100

    styles

--- qt/core/Application.hx
+++ qt/core/Application.hx
@@ -1,6 +1,8 @@
 package qt.core;
 
 import cpp.Pointer;
+import cpp.RawPointer;
+import qt.styles.Style;
 
 @:include('QtWidgets/qapplication.h')
 class Application {
@@ -12,8 +14,22 @@ class Application {
         _ref = untyped __cpp__('new QApplication(argc, argv)');
     }
     
+    public static var style(null, set):Style;
+    private static function set_style(value:Style):Style {
+        QApplication.setStyle(value.styleRef.get_raw());
+        return value;
+    }
+    
     public function exec() {
         _ref.ptr.exec();
+    }
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Helpers
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public var applicationRef(get, null):Pointer<QApplication>;
+    private function get_applicationRef():Pointer<QApplication> {
+        return _ref.reinterpret();
     }
 }
 
@@ -23,6 +39,8 @@ class Application {
 @:unreflective
 @:include('QtWidgets/QApplication.h')
 @:native('QApplication')
+@:structAccess
 extern class QApplication {
     public function exec():Void;
+    @:native("QApplication::setStyle") public static function setStyle(style:RawPointer<qt.styles.Style.QStyle>):Void;
 }
---
+++ qt/styles/StyleFactory.hx
@@ -0,0 +1,26 @@
+package qt.styles;
+
+import cpp.Pointer;
+import cpp.RawPointer;
+import cpp.Reference;
+import qt.core.QString;
+import qt.styles.Style.QStyle;
+
+class StyleFactory {
+    public static function create(key:String):Style {
+        var pointer:RawPointer<QStyle> = QStyleFactory.create(qt.core.QString.Helper.fromString(key));
+        var style = new Style();
+        style._ref = Pointer.fromRaw(pointer).reinterpret();
+        return style;
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Extern
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+@:unreflective
+@:include('QtWidgets/QStyleFactory.h')
+@:native('QStyleFactory')
+extern class QStyleFactory {
+    @:native('QStyleFactory::create') public static function create(key:Reference<QString>):RawPointer<QStyle>;
+}
